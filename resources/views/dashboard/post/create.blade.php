147c731b863206814e622efd387b634680f9746f
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-12 12:01:25 -0600

    Feat :sparkles: Show mistakes in the form

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -1,9 +1,19 @@
 @extends('dashboard.master')
 
 @section('content')
+
+
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <div>
+                {{ $error }} 
+            </div>
+        @endforeach
+    @endif
+
     <form action="{{ route('post.store') }}" method="post">
         @csrf
-        
+
         <label for="title">Title</label>
         <input type="text" name="title" id="title">
 
